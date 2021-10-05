43fe6b2d6e7666cdd4ed88b5c5a0ba5cf39d2b25
Davidpavan <David@zelarsoft.com>
2021-10-05 18:24:08 +0530

    Commit

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -138,3 +138,18 @@ resource "aws_route_table" "private-route-table" {
     Name     = "Private Route Table"
   }
 }
+
+resource "aws_route_table_association" "private-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-1.id
+  route_table_id      = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-2.id
+  route_table_id      = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-3-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-3.id
+  route_table_id      = aws_route_table.private-route-table.id
+}
