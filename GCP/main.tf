3046d2c48134322818b2be1358abc8fc36777fea
Davidpavan <David@zelarsoft.com>
2021-10-06 22:29:58 +0530

    Commit

---
+++ GCP/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("C:\Users\new\imperial-ally-328216-0a85dcd14543.json")
+  project = "imperial-ally-328216"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
